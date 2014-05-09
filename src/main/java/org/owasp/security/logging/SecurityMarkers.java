4530c14305d424c1ecbe4efebd572b7811c0b4b8
August Detlefsen <adetlefsen@appsecconsulting.com>
2014-05-09 15:11:31 -0700

    Added Markers for Security event types
    Mimicing the EventType objects from ESAPI logging

--- src/main/java/org/owasp/security/logging/SecurityMarkers.java
+++ src/main/java/org/owasp/security/logging/SecurityMarkers.java
@@ -15,6 +15,14 @@ public class SecurityMarkers {
 	public static final String CONFIDENTAL_MARKER_NAME = "CONFIDENTIAL";
 	public static final String SECRET_MARKER_NAME = "SECRET";
 	public static final String TOP_SECRET_MARKER_NAME = "TOPSECRET";
+	
+	public static final String SECURITY_SUCCESS_MARKER_NAME = "SECURITY SUCCESS";
+	public static final String SECURITY_FAILURE_MARKER_NAME = "SECURITY FAILURE";
+	public static final String SECURITY_AUDIT_MARKER_NAME = "SECURITY AUDIT";
+
+	public static final String EVENT_SUCCESS_MARKER_NAME = "EVENT SUCCESS";
+	public static final String EVENT_FAILURE_MARKER_NAME = "EVENT FAILURE";
+	public static final String EVENT_UNSPECIFIED_MARKER_NAME = "EVENT UNSPECIFIED";
 
 	public static Marker SECURITY_MARKER = MarkerFactory
 			.getMarker(SECURITY_MARKER_NAME);
@@ -31,4 +39,16 @@ public class SecurityMarkers {
 	public static Marker TOP_SECRET_MARKER = MarkerFactory
 			.getMarker(TOP_SECRET_MARKER_NAME);
 
+	public static Marker SECURITY_SUCCESS = MarkerFactory.getMarker(SECURITY_SUCCESS_MARKER_NAME);
+
+	public static Marker SECURITY_FAILURE = MarkerFactory.getMarker(SECURITY_FAILURE_MARKER_NAME);
+
+	public static Marker SECURITY_AUDIT = MarkerFactory.getMarker(SECURITY_AUDIT_MARKER_NAME);
+
+	public static Marker EVENT_SUCCESS = MarkerFactory.getMarker(EVENT_SUCCESS_MARKER_NAME);
+
+	public static Marker EVENT_FAILURE = MarkerFactory.getMarker(EVENT_FAILURE_MARKER_NAME);
+
+	public static Marker EVENT_UNSPECIFIED = MarkerFactory.getMarker(EVENT_UNSPECIFIED_MARKER_NAME);
+	
 }
