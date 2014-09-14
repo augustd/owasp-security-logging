cefd2c33851aa3285eb534b3604f19bb6cf2d109
unknown <java.development@gmail.com>
2014-09-14 20:28:37 +0200

    code clean up

--- src/main/java/org/owasp/security/logging/layout/SecurityLoggingLayout.java
+++ src/main/java/org/owasp/security/logging/layout/SecurityLoggingLayout.java
@@ -5,11 +5,15 @@ import org.slf4j.MDC;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.LayoutBase;
-
+/**
+ * 
+ * Layout for security related logging 
+ *
+ */
 public class SecurityLoggingLayout extends LayoutBase<ILoggingEvent> {
 
 	private static final Object LINE_SEP = System.getProperty("line.separator");
-	private String prefix = "layout";
+	private String prefix = "Security";
 	private boolean printThreadName = true;
 
 	public String doLayout(ILoggingEvent event) {
@@ -21,6 +25,7 @@ public class SecurityLoggingLayout extends LayoutBase<ILoggingEvent> {
 				- event.getLoggerContextVO().getBirthTime());
 		sbuf.append(" ");
 		sbuf.append(event.getLevel());
+		sbuf.append(" ");
 		sbuf.append(event.getLoggerName());
 		sbuf.append(" - ");
 		sbuf.append(MDC.get(MDCFilter.LOGIN_ID));
--- src/test/java/org/owasp/security/logging/filter/MarkerFilterTest.java
+++ src/test/java/org/owasp/security/logging/filter/MarkerFilterTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.owasp.security.logging.SecurityMarkers;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.LoggingEvent;
@@ -29,5 +30,8 @@ public class MarkerFilterTest {
 		assertEquals(FilterReply.DENY, mkt.decide(nulEvent));
 		// assertEquals(FilterReply.ACCEPT,
 		// mkt.decide(SECURITY_MARKER, null, null, null, null, null));
+		Logger LOGGER = lc.getLogger(MarkerFilterTest.class);
+		LOGGER.debug(SecurityMarkers.TOP_SECRET_MARKER, "This is so right !");
+		LOGGER.debug(SecurityMarkers.CONFIDENTIAL_MARKER, "Look at this !");
 	}
 }
