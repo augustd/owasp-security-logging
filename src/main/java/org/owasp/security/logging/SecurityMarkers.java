0d7c738a20b4a90e9b1874fcc99cb3fec1345243
unknown <java.development@gmail.com>
2014-12-09 21:45:30 +0100

    added Coverity scan to Travis build

--- src/main/java/org/owasp/security/logging/SecurityMarkers.java
+++ src/main/java/org/owasp/security/logging/SecurityMarkers.java
@@ -15,7 +15,7 @@ public class SecurityMarkers {
 	public static final String CONFIDENTAL_MARKER_NAME = "CONFIDENTIAL";
 	public static final String SECRET_MARKER_NAME = "SECRET";
 	public static final String TOP_SECRET_MARKER_NAME = "TOPSECRET";
-	
+
 	public static final String SECURITY_SUCCESS_MARKER_NAME = "SECURITY SUCCESS";
 	public static final String SECURITY_FAILURE_MARKER_NAME = "SECURITY FAILURE";
 	public static final String SECURITY_AUDIT_MARKER_NAME = "SECURITY AUDIT";
@@ -39,16 +39,22 @@ public class SecurityMarkers {
 	public static Marker TOP_SECRET_MARKER = MarkerFactory
 			.getMarker(TOP_SECRET_MARKER_NAME);
 
-	public static Marker SECURITY_SUCCESS = MarkerFactory.getMarker(SECURITY_SUCCESS_MARKER_NAME);
+	public static Marker SECURITY_SUCCESS = MarkerFactory
+			.getMarker(SECURITY_SUCCESS_MARKER_NAME);
 
-	public static Marker SECURITY_FAILURE = MarkerFactory.getMarker(SECURITY_FAILURE_MARKER_NAME);
+	public static Marker SECURITY_FAILURE = MarkerFactory
+			.getMarker(SECURITY_FAILURE_MARKER_NAME);
 
-	public static Marker SECURITY_AUDIT = MarkerFactory.getMarker(SECURITY_AUDIT_MARKER_NAME);
+	public static Marker SECURITY_AUDIT = MarkerFactory
+			.getMarker(SECURITY_AUDIT_MARKER_NAME);
 
-	public static Marker EVENT_SUCCESS = MarkerFactory.getMarker(EVENT_SUCCESS_MARKER_NAME);
+	public static Marker EVENT_SUCCESS = MarkerFactory
+			.getMarker(EVENT_SUCCESS_MARKER_NAME);
 
-	public static Marker EVENT_FAILURE = MarkerFactory.getMarker(EVENT_FAILURE_MARKER_NAME);
+	public static Marker EVENT_FAILURE = MarkerFactory
+			.getMarker(EVENT_FAILURE_MARKER_NAME);
 
-	public static Marker EVENT_UNSPECIFIED = MarkerFactory.getMarker(EVENT_UNSPECIFIED_MARKER_NAME);
-	
+	public static Marker EVENT_UNSPECIFIED = MarkerFactory
+			.getMarker(EVENT_UNSPECIFIED_MARKER_NAME);
+
 }
