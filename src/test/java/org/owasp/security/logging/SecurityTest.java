541cd4984ef0c09e0372be396748cb85a635c124
August Detlefsen <augustd@codemagi.com>
2014-12-15 19:08:26 -0800

    Added new test for log injection
    (This test presently fails)

---
+++ src/test/java/org/owasp/security/logging/SecurityTest.java
@@ -0,0 +1,71 @@
+package org.owasp.security.logging;
+
+import ch.qos.logback.access.PatternLayoutEncoder;
+import org.junit.Test;
+import org.mockito.Mock;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.After;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import static org.mockito.Mockito.verify;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.LoggerFactory;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SecurityTest {
+
+    LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+    Logger LOGGER;
+
+    @Mock
+    private RollingFileAppender mockAppender = new RollingFileAppender();
+
+    //Captor is genericised with ch.qos.logback.classic.spi.LoggingEvent
+    @Captor
+    private ArgumentCaptor<LoggingEvent> captorLoggingEvent;
+
+    @Before
+    public void setup() {
+        // mockAppender = new RollingFileAppender();
+        mockAppender.setContext(loggerContext);
+        mockAppender.setFile("testFile.log");
+
+        PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+        encoder.setContext(loggerContext);
+        encoder.setPattern("%-4relative [%thread] %-5level %logger{35} - %msg%n");
+        encoder.start();
+
+        mockAppender.setEncoder(encoder);
+        mockAppender.start();
+        LOGGER = loggerContext.getLogger("Main");
+        LOGGER.addAppender(mockAppender);
+
+    }
+
+    @After
+    public void teardown() {
+        LOGGER.detachAppender(mockAppender);
+    }
+
+    @Test
+    public void injectionTest() {
+
+        LOGGER.info("This message contains \r\n line feeds");
+
+        //Now verify our logging interactions
+        verify(mockAppender).doAppend(captorLoggingEvent.capture());
+
+        //Get the logging event from the captor
+        final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+
+        System.out.println("MESSAGE: " + loggingEvent.getFormattedMessage());
+        //assertThat(loggingEvent.getFormattedMessage(), is("This message contains line feeds"));
+    }
+}
